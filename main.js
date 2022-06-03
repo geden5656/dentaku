96bf979d22807cf39d50b91553f7effd21b3ed2e
EC2 Default User <ec2-user@ip-172-31-10-63.ec2.internal>
2022-06-03 10:54:39 +0000

    dentaku

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function clickbutton(target){
   let result = document.getElementById("result")
   let target_value=target.innerHTML;
-  
   if (target_value == "AC") {
     result.innerHTML="0"
   }else if(target_value=="="){
@@ -11,6 +10,22 @@ function clickbutton(target){
       result.innerHTML=target_value
     }else{
     result.innerHTML+=target_value
-    }
+    } 
   }
+}  
+function opebutton(target){
+  let result = document.getElementById("result")
+  let target_value=target.innerHTML;
+  if(result.innerHTML.slice(-1)==="+"){
+    return;
+  }else if(result.innerHTML.slice(-1)==="-"){
+    return;
+  }else if(result.innerHTML.slice(-1)==="*"){
+    return;
+  }else if(result.innerHTML.slice(-1)==="/"){
+    return;
+  }else{
+    result.innerHTML+=target_value
+  }
+  
 }
